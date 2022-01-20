f6669caa2fed66e94f9f4b100712b060dacb4232
DQ-StarKiller <diequintanilla@gmail.com>
2022-01-19 21:42:17 -0600

    UPDATE TO ALL FILES
    UPDATE:
    - add Jquery CDN to html file
    - remove background color from page (ongoing issue with page height)
    - add Jquery code for start and stop buttons
    - add Jquery code to wait for DOM to load

--- tracker.js
+++ tracker.js
@@ -1,37 +1,55 @@
-const startButton = document.querySelector('#startButton');
-const stopButton = document.querySelector('#stopButton');
+$(document).ready(function() {
+	console.log('DOM READY!');
+});
+
+// const startButton = document.querySelector('#startButton');
+// const stopButton = document.querySelector('#stopButton');
 const ul = document.querySelector('#returnedContent');
 
 let total = 0;
 
 const fetchBitcoinPrice = async () => {
-    try {
-        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-        // console.log(`Price of Bitcoin: $${res.data.ticker.price}`)
-        // console.log(`Change in price: ${res.data.ticker.change}%`)
-        const testLI = document.createElement('li');
-        const info = testLI.innerText = `Price of Bitcoin: $${res.data.ticker.price}
-        Change in price: ${res.data.ticker.change}%`
-        const cls = testLI.classList.add('box')
-        ul.appendChild(testLI, info, cls)
-    } catch (e) {
-        console.log("ERROR!", e)
-    }
-}
+	try {
+		const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd');
+		// console.log(`Price of Bitcoin: $${res.data.ticker.price}`)
+		// console.log(`Change in price: ${res.data.ticker.change}%`)
+		const testLI = document.createElement('li');
+		const info = (testLI.innerText = `Price of Bitcoin: $${res.data.ticker.price}
+        Change in price: ${res.data.ticker.change}%`);
+		const cls = testLI.classList.add('box');
+		ul.appendChild(testLI, info, cls);
+	} catch (e) {
+		console.log('ERROR!', e);
+	}
+};
+
+// let ping = setInterval(() => {
+// 	fetchBitcoinPrice();
+// 	total += 1;
+// }, 900000);
 
 let ping = setInterval(() => {
-    fetchBitcoinPrice();
-    total += 1;
-}, 900000);
+	fetchBitcoinPrice();
+	total += 1;
+}, 30000);
 
-startButton.addEventListener('click', () => {
-    ping;
-})
+// startButton.addEventListener('click', () => {
+// 	ping;
+// });
 
-stopButton.addEventListener('click', function(){
-    clearInterval(ping)
-    console.log(`USER REQUESTED HALT. SERVER PINGED ${total} TIMES`)
-})
+$('#startButton').on('click', function() {
+	ping;
+});
+
+// stopButton.addEventListener('click', function() {
+// 	clearInterval(ping);
+// 	console.log(`USER REQUESTED HALT. SERVER PINGED ${total} TIMES`);
+// });
+
+$('#stopButton').on('click', function() {
+	clearInterval(ping);
+	console.log(`USER REQUESTED HALT. SERVER PINGED ${total} TIMES`);
+});
 
 // TODO
 // ADD FUNCTION FOR LINES 12-16 TO DE-CLUTTER ASYNC FUNCTION
